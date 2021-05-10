9af0df42d8050deef839ffc1fcb23fa43278bc4d
Rustam Yuburov <rt138328@gmail.com>
2021-05-10 19:37:16 +0600

    Added eventlisteners to buttons

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 import { init } from './modules/initial-page';
 
 init();
+
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach(button => button.addEventListener('click', 'loadPage'));
+
+// // function loadPage(e) {
+// //     const page = e.target.
+// // }
